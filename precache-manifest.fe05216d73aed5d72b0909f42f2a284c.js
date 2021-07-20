c6c5d93d5497959b6e801e532e797c4d146848a9
Ben Sadeghi <ben.sadeghi1@gmail.com>
2021-07-21 06:12:35 +0800

    Updates

---
+++ precache-manifest.fe05216d73aed5d72b0909f42f2a284c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4cf0ce1a6b49f18adb5ecf04ce7a90",
+    "url": "/tala.finance/index.html"
+  },
+  {
+    "revision": "3fde5e3f81343770ab5a",
+    "url": "/tala.finance/static/css/main.a9873e29.chunk.css"
+  },
+  {
+    "revision": "ebc71f3ad3564303fe41",
+    "url": "/tala.finance/static/js/2.f6832e3d.chunk.js"
+  },
+  {
+    "revision": "4d92265ee4745234cbf941e19ca6c02e",
+    "url": "/tala.finance/static/js/2.f6832e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fde5e3f81343770ab5a",
+    "url": "/tala.finance/static/js/main.28f266e1.chunk.js"
+  },
+  {
+    "revision": "c475366113e83f42381b",
+    "url": "/tala.finance/static/js/runtime-main.34d66361.js"
+  }
+]);
